89307275dbedbac449d3cc1c77ab1efdfb195036
serhiivostrikov <serhii_vostrikov@thomsonreuters.com>
2022-01-17 17:35:41 +0200

    chore improvements

--- utils/helpers/take.screenshot.js
+++ utils/helpers/take.screenshot.js
@@ -1,14 +1,14 @@
 const { access, constants } = require('fs');
 
-async function takeScreenshot(path, image) {
+const takeScreenshot = async (path, image) => {
 
   access(path, constants.F_OK, (err) => {
     if (err) {
-      console.info("File path doesn't exists ... Let's create!");
+      console.info(`File path ${path} doesn't exists ... Let's create!`);
       fs.mkdir(path, {recursive: true}, err => {});
     }
   });
-  await browser.saveScreenshot(path + image);
+  await browser.saveScreenshot(`${path}${image}`);
 }
 
-module.exports = { takeScreenshot }
+module.exports = { takeScreenshot };
